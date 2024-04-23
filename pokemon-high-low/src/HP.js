dceb5437b59bafcc4546a2c737154981afc08e67
thoo2002 <timmyhoo88@gmail.com>
2024-04-23 11:07:53 -0400

    updated hp file

--- pokemon-high-low/src/HP.js
+++ pokemon-high-low/src/HP.js
@@ -1,7 +1,34 @@
+import React, { useState, useEffect } from 'react';
+
 
 function HP() {
+    const [data, setData] = useState(null);
+    const generateRandomNumber = () => {
+        const min = 1;
+        const max = 1025;
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    const [rand, setRand] = useState(generateRandomNumber());
+    useEffect(() => {
+        fetch('https://v1/pokemon/stats/'+ rand)
+            .then((response) => response.json())
+            .then((json) => setData(json.attributes.base_hp))
+            .catch(error => console.log(error));
+    }, []);
+
     return (
-        <p>test</p>
+        <div>
+            {data ? (
+                <ul>
+                    {data.map(item => (
+                        <li key={item.id}>{item.name}</li>
+                    ))}
+                </ul>
+            ) : (
+                <p>Loading data...</p>
+            )}
+        </div>
     );
 }
 
