0e0c82e59f20fa8be3da8bc1d9dfaa91637b9172
thoo2002 <timmyhoo88@gmail.com>
2024-04-23 11:16:33 -0400

    update to hp file

--- pokemon-high-low/src/HP.js
+++ pokemon-high-low/src/HP.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react';
 
 
 function HP() {
+    var token = ZLfYaxdGF3Y3W5VX5pKfwSgC8gyoFexpROaxdAbaAqzPT8wlT3d9GRD2WYx9kS9K
     const [data, setData] = useState(null);
+    const [name, setName] = useState(null);
     const generateRandomNumber = () => {
         const min = 1;
         const max = 1025;
@@ -11,9 +13,10 @@ function HP() {
 
     const [rand, setRand] = useState(generateRandomNumber());
     useEffect(() => {
-        fetch('https://v1/pokemon/stats/'+ rand)
+        fetch('http://api.pkmnapi.com/v1/pokemon/stats/'+ rand,{ headers:{'Authorization': 'Bearer ' + token}})
             .then((response) => response.json())
             .then((json) => setData(json.attributes.base_hp))
+            .then((json) => setName(json.attributes.name))
             .catch(error => console.log(error));
     }, []);
 
