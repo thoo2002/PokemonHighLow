480637c0364d45dadbf7761834a0508d808d922e
alexed154 <aedmon01@villanova.edu>
2024-04-25 11:00:17 -0400

    Working app.js

--- pokemon-high-low/src/App.js
+++ pokemon-high-low/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HP from './HP.js';
 import ATT from './ATT.js';
 import DEF from './DEF.js';
@@ -8,48 +8,43 @@ import SPDEF from './SPDEF.js';
 import SPEED from './SPEED.js';
 
 function App() {
-
-  const navigate = useNavigate();
-
   return (
-    <Router>
-    <div className="App">
-      <header className="App-header">
-        <h1>Pokemon High Low Game</h1>
-        <nav>
-          <ul>
-            <li>
-              <button onClick={() => window.location.href='/HP'}>HP</button>
-            </li>
-            <li>
-              <button onClick={() => window.location.href='/ATT'}>ATTACK</button>
-            </li>
-            <li>
-              <button onClick={() => window.location.href='/DEF'}>DEFENSE</button>
-            </li>
-            <li>
-              <button onClick={() => window.location.href='/SPATT'}>SPECIAL ATTACK</button>
-            </li>
-            <li>
-              <button onClick={() => window.location.href='/SPDEF'}>SPECIAL DEFENSE</button>
-            </li>
-            <li>
-              <button onClick={() => window.location.href='/SPEED'}>SPEED</button>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <div className="App">
+        <header className="App-header">
+          <h1>Pokemon High Low Game</h1>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/HP">HP</Link>
+              </li>
+              <li>
+                <Link to="/ATT">ATTACK</Link>
+              </li>
+              <li>
+                <Link to="/DEF">DEFENSE</Link>
+              </li>
+              <li>
+                <Link to="/SPATT">SPECIAL ATTACK</Link>
+              </li>
+              <li>
+                <Link to="/SPDEF">SPECIAL DEFENSE</Link>
+              </li>
+              <li>
+                <Link to="/SPEED">SPEED</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
 
-      <Routes>
-        <Route path="/HP" element={<HP />} />
-        <Route path="/ATT" element={<ATT />} />
-        <Route path="/DEF" element={<DEF />} />
-        <Route path="/SPATT" element={<SPATT />} />
-        <Route path="/SPDEF" element={<SPDEF />} />
-        <Route path="/SPEED" element={<SPEED />} />
-      </Routes>
-    </div>
-  </Router>
+        <Routes>
+          <Route path="/HP" element={<HP />} />
+          <Route path="/ATT" element={<ATT />} />
+          <Route path="/DEF" element={<DEF />} />
+          <Route path="/SPATT" element={<SPATT />} />
+          <Route path="/SPDEF" element={<SPDEF />} />
+          <Route path="/SPEED" element={<SPEED />} />
+        </Routes>
+      </div>
   );
 }
 
