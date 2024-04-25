9bde9821297b5e8ddabd2eb5a1139dc2db31444a
thoo2002 <timmyhoo88@gmail.com>
2024-04-25 10:38:21 -0400

    fixed functions

--- pokemon-high-low/src/HP.js
+++ pokemon-high-low/src/HP.js
@@ -2,12 +2,27 @@ import React, { useState, useEffect } from 'react';
 
 
 function HP() {
-    var token = ZLfYaxdGF3Y3W5VX5pKfwSgC8gyoFexpROaxdAbaAqzPT8wlT3d9GRD2WYx9kS9K
+    var token = 'ZLfYaxdGF3Y3W5VX5pKfwSgC8gyoFexpROaxdAbaAqzPT8wlT3d9GRD2WYx9kS9K';
     const [data, setData] = useState(null);
     const [name, setName] = useState(null);
     const [img, setImg] = useState(null);
-    const [pkm1, setPkm1] = useState(null);
-    const [pkm2, setPkm2] = useState(null);
+    const [pkm1, setPkm1] = useState({
+        name: 'bulbasaur',
+        stat: 45,
+        img: 'none',
+    });
+    useEffect(() => {
+        setPkm1({name: getPkmnName(),
+            stat: getPkmnStat(),
+            img: getPkmnImg(),
+        }
+    )});
+
+    const [pkm2, setPkm2] = useState({
+        name: 'bulbasaur',
+        stat: 45,
+        img: 'none',
+    });
     const [counter, setCounter] = useState(0);
 
     const generateRandomNumber = () => {
@@ -17,52 +32,64 @@ function HP() {
     };
 
     const [rand, setRand] = useState(generateRandomNumber());
-    getPkmnStat(() => {
+    function getPkmnStat(){
         fetch('http://api.pkmnapi.com/v1/pokemon/stats/'+ rand,{ headers:{'Authorization': 'Bearer ' + token}})
             .then((response) => response.json())
-            .then((json) => setData(json.data[].attributes.base_hp))
+            .then((json) => setData(json.data[0].attributes.base_hp))
             .catch(error => console.log(error));
-    }, []);
+    };
 
-    getPkmnImg(() => {
+    function getPkmnImg() {
         fetch('http://api.pkmnapi.com/v1/pokemon/pics'+ rand,{ headers:{'Authorization': 'Bearer ' + token}})
             .then((response) => response.json())
-            .then((json) => setImg(json.data[].attributes.front))
+            .then((json) => setImg(json.data[0].attributes.front))
             .catch(error => console.log(error));
-    }, []);
+    };
 
-    getPkmnName(() => {
+    function getPkmnName() {
         fetch('http://api.pkmnapi.com/v1/pokemon/names/'+ rand,{ headers:{'Authorization': 'Bearer ' + token}})
             .then((response) => response.json())
-            .then((json) => setName(json.data[].attributes.name))
+            .then((json) => setName(json.data[0].attributes.name))
             .catch(error => console.log(error));
-    },[]);
+    };
 
-    ComparisonHigher(() => {
+    function ComparisonHigher() {
         if (pkm1 >= pkm2){
             setCounter(counter + 1);
             setPkm1(pkm2);
+            setPkm2({
+                name: getPkmnName(),
+                stat: getPkmnStat(),
+                img: getPkmnImg(),
+            });
         }
         else{
             setCounter(0);
-            print ("You lose");
         }
-    },[])
+    }
 
-    ComparisonLower(() =>{
+    function ComparisonLower(){
         if (pkm1 <= pkm2){
             setCounter(counter + 1);
             setPkm1(pkm2);
+            setPkm2({
+                name: getPkmnName(),
+                stat: getPkmnStat(),
+                img: getPkmnImg(),
+            });
         }
         else{
             setCounter(0);
-            print ("You lose");
         }
-    },[])
+    }
     return (
         <div>
-            <button onClick={() => ComparisonHigher()}>Higher</button>
-            <button onClick={() => ComparisonLower()}>Lower</button>
+            <h1>High Low</h1>
+            <h2>{pkm1.name}</h2>
+            <img src={pkm1.img} alt="Pokemon" />
+            {/* <button onClick={() => ComparisonHigher()}>Higher</button>
+            <button onClick={() => ComparisonLower()}>Lower</button> */}
+            <h2>Score: {counter}</h2>
         </div>
     );
 }
